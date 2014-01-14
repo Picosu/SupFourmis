4851ccd7ce617c917620a7e7949384dced0b8635
maxence <maxence.decussac@hotmail.fr>
2014-01-14 14:49:58 +0100

    correction methode stats de fourmiliere

--- SupFourmis/Fourmiliere.cpp
+++ SupFourmis/Fourmiliere.cpp
@@ -36,27 +36,30 @@ int Fourmiliere::get_nb()
 
 void Fourmiliere::stats()
 {
-    int min(0), max(0);
+    int min(0), max(0), indexMinFourmis(0), indexMaxFourmis(0);
     // two choices are available, either from 0 to the size() -1 or from the beginning to the end
     std::vector<Fourmi>::iterator i;
     for (i=popu.begin(); i!=popu.end(); i++)
     {
         if (min > (*i).Fourmi::get_d())
         {
+            indexMinFourmis = (int)std::distance(popu.begin(), i);
             min = (*i).Fourmi::get_d();
         }
         if (max < (*i).Fourmi::get_d())
         {
+            indexMaxFourmis = (int)std::distance(popu.begin(), i);
             max = (*i).Fourmi::get_d();
         }
     }
-    std::cout << "min : " << min << " max : " << max << std::endl;
+    std::cout << "Distance minimale : " << min << " parcourue par la fourmi : " << indexMinFourmis  << std::endl;
+    std::cout << "Distance maximale : " << max << " parcourue par la fourmi : " << indexMaxFourmis  << std::endl;
 }
 
 void Fourmiliere::stats(int n, int m)
 {
     // Testing if the two values are ordered, if they're not, order them.
-    int min(0),max(0);
+    int min(0),max(0), indexMinFourmis(0), indexMaxFourmis(0);
     if (n > m)
     {
         int temp = m;
@@ -69,12 +72,17 @@ void Fourmiliere::stats(int n, int m)
         int value = popu[i].get_d();
         if (min > value)
         {
+            indexMinFourmis = i;
             min = value;
         }
         if (max < value)
         {
+            indexMaxFourmis = i;
             max = value;
         }
     }
+    std::cout << "Distance minimale : " << min << " parcourue par la fourmi : " << indexMinFourmis  << std::endl;
+    std::cout << "Distance maximale : " << max << " parcourue par la fourmi : " << indexMaxFourmis  << std::endl;
+
     std::cout << "min : " << min << " max : " << max << std::endl;
 }
